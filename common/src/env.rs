61cf49b3c00abbe2646ed891e65b1f5d1208ea3c
Danny <danny@tatom.xyz>
2024-09-11 11:03:58 -0700

    feat(authn): use env var for redis config (#41)
    closes #29

--- common/src/env.rs
+++ common/src/env.rs
@@ -3,13 +3,14 @@ use serde::{Deserialize, Deserializer};
 
 #[derive(Clone, Deserialize)]
 pub struct Environment {
+    pub development: bool,
     #[serde(deserialize_with = "string_to_secstr")]
     pub master_key: SecStr,
     pub postgres_url: String,
     pub postgres_pool_size: u32,
+    pub redis_url: String,
     pub resolver_host: String,
     pub authn_base_url: String,
-    pub development: bool,
 }
 
 pub fn string_to_secstr<'de, D>(deserializer: D) -> Result<SecStr, D::Error>
@@ -23,12 +24,13 @@ where
 impl Default for Environment {
     fn default() -> Self {
         Self {
+            development: Default::default(),
             master_key: SecStr::new(vec![]),
             postgres_url: Default::default(),
             postgres_pool_size: Default::default(),
+            redis_url: Default::default(),
             resolver_host: Default::default(),
             authn_base_url: Default::default(),
-            development: Default::default(),
         }
     }
 }
