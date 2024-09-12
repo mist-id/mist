b3bf7089b4d3cf620224f769d0c32b6ba1aa3b9f
Danny Tatom <danny@tatom.xyz>
2024-09-12 15:25:45 -0700

    chore(common): set default values for env vars
    closes #21

--- common/src/env.rs
+++ common/src/env.rs
@@ -3,14 +3,19 @@ use serde::{Deserialize, Deserializer};
 
 #[derive(Clone, Deserialize)]
 pub struct Environment {
-    pub development: bool,
     #[serde(deserialize_with = "string_to_secstr")]
     pub master_key: SecStr,
-    pub postgres_url: String,
-    pub postgres_pool_size: u32,
-    pub redis_url: String,
-    pub resolver_host: String,
     pub authn_base_url: String,
+    #[serde(default = "default_postgres_url")]
+    pub postgres_url: String,
+    #[serde(default = "default_postgres_pool_size")]
+    pub postgres_pool_size: u32,
+    #[serde(default = "default_redis_url")]
+    pub redis_url: String,
+    #[serde(default = "default_resolver_host")]
+    pub resolver_host: String,
+    #[serde(default)]
+    pub development: bool,
 }
 
 pub fn string_to_secstr<'de, D>(deserializer: D) -> Result<SecStr, D::Error>
@@ -19,6 +24,22 @@ where
 {
     let s: String = Deserialize::deserialize(deserializer)?;
     Ok(SecStr::new(s.into_bytes()))
+}
+
+fn default_postgres_url() -> String {
+    "postgres://casper@localhost/mist".into()
+}
+
+fn default_postgres_pool_size() -> u32 {
+    10
+}
+
+fn default_redis_url() -> String {
+    "redis://localhost".into()
+}
+
+fn default_resolver_host() -> String {
+    "http://localhost:9003/1.0/identifiers".into()
 }
 
 impl Default for Environment {
